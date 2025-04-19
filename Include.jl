53faa06ec0b7af6c183c557296b02749250fd3f9
Jeffrey Varner <jdv27@cornell.edu>
2025-04-19 13:31:13 -0400

    running
    running

--- Include.jl
+++ Include.jl
@@ -1,6 +1,7 @@
 # setup paths -
 const _ROOT = pwd();
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
 
 # check do we have a Manifest.toml file?
 using Pkg;
@@ -18,8 +19,10 @@ using CSV
 using FileIO
 using Flux
 using NNlib
+using OneHotArrays
+using Random
+using JLD2
 
 # load my codes -
-# include(joinpath(_PATH_TO_SRC, "Types.jl"));
-# include(joinpath(_PATH_TO_SRC, "Files.jl"));
-# include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
