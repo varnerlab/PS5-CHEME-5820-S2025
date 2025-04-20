2a35121642ec5cbaa104154f6385d76177ac85c9
Jeffrey Varner <jdv27@cornell.edu>
2025-04-20 07:33:53 -0400

    Running
    Running

--- Include.jl
+++ Include.jl
@@ -3,6 +3,9 @@ const _ROOT = pwd();
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 
+# flag to check if the include file was called -
+const _DID_INCLUDE_FILE_GET_CALLED = true;
+
 # check do we have a Manifest.toml file?
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
@@ -22,6 +25,7 @@ using NNlib
 using OneHotArrays
 using Random
 using JLD2
+using Test
 
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"));
