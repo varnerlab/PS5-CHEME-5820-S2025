71d1fb342e819e97db402e0e893b9a3789c2737e
Jeffrey Varner <jdv27@cornell.edu>
2026-03-17 20:13:59 -0400

    PS5

--- src/Types.jl
+++
@@ -1,6 +0,0 @@
-abstract type AbstractNeuralNetwork end
-
-# build a simple neural network model type -
-struct MyFluxNeuralNetworkModel <: AbstractNeuralNetwork
-    chain::Chain; # holds the model chain
-end
